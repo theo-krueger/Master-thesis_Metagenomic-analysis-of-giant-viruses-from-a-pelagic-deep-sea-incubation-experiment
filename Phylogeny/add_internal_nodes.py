dd881a8b1e1a9b1d1e6515d0b3b793afb57336d5
theo-krueger <theokrueger.marbio@gmail.com>
2023-07-19 09:17:41 +0200

    better readability

--- Phylogeny/add_internal_nodes.py
+++ Phylogeny/add_internal_nodes.py
@@ -2,20 +2,26 @@ import csv
 from ete3 import Tree
 import argparse
 
-argParser = argparse.ArgumentParser()
 
-argParser.add_argument("-i", "--input-file", help="Input path to the tree file", required=True)
-argParser.add_argument("-o", "--output-path", help="Output path for new tree")
-argParser.add_argument("--bootstrap-table", help="Provide bootstrap value table", action="store_true")
+def get_args(argv=None):
+    argParser = argparse.ArgumentParser()
 
-args = argParser.parse_args()
+    argParser.add_argument("-i", "--input-file", help="Input path to the tree file", required=True)
+    argParser.add_argument("-o", "--output-path", help="Output path for new tree")
+    argParser.add_argument("--bootstrap-table", help="Provide bootstrap value table", action="store_true")
+
+    return argParser.parse_args(argv)
+
+
+def read_tree(args):
+    return Tree(args.input_file)
 
 
 def assign_node_ids(node, counter, bootstrap_mapping):
     if node.is_leaf():
         return counter
     else:
-        node.name = "InternalNode_" + str(counter)
+        node.name = "InternalNode_" + str(counter).zfill(4)
         counter += 1
         for child in node.children:
             counter = assign_node_ids(child, counter, bootstrap_mapping)
@@ -23,27 +29,34 @@ def assign_node_ids(node, counter, bootstrap_mapping):
         return counter
 
 
-tree = Tree(args.input_file)
+def write_output(args, tree):
+    if args.output_path:
+        output_file_tree_name = args.input_file.split("/")[-1].split(".")[0]
+        output_file_tree = f"{args.args.output_path}{output_file_tree_name}.nwk"
+    else:
+        output_file_tree = f"{args.input_file.split('.')[0]}_AddIntNodes.nwk"
 
-bootstrap_mapping = {}
-assign_node_ids(tree, 1, bootstrap_mapping)
+    tree.write(format=1, outfile=output_file_tree)
+    print("Tree successfully exported to: ", output_file_tree)
 
-if args.output_path:
-    output_file_tree_name = args.input_file.split("/")[-1].split(".")[0]
-    output_file_tree = f"{args.args.output_path}{output_file_tree_name}.nwk"
-else:
-    output_file_tree = f"{args.input_file.split('.')[0]}_AddIntNodes.nwk"
+    if args.bootstrap_table:
+        output_file_table = f"{output_file_tree.split('.')[0]}_bootstraps.tsv"
 
-tree.write(format=1, outfile=output_file_tree)
-print("Tree successfully exported to: ", output_file_tree)
+        with open(output_file_table, mode="w", newline="") as file:
+            writer = csv.writer(file, delimiter="\t")
+            writer.writerow(["Internal_node_ID", "Bootstrap_value"])
+            for node_id, bootstrap_value in bootstrap_mapping.items():
+                writer.writerow([node_id, bootstrap_value])
 
-if args.bootstrap_table:
-    output_file_table = f"{output_file_tree.split('.')[0]}_bootstraps.tsv"
+        print("Table successfully exported to: ", output_file_table)
 
-    with open(output_file_table, mode="w", newline="") as file:
-        writer = csv.writer(file, delimiter="\t")
-        writer.writerow(["Internal_node_ID", "Bootstrap_value"])
-        for node_id, bootstrap_value in bootstrap_mapping.items():
-            writer.writerow([node_id, bootstrap_value])
 
-    print("Table successfully exported to: ", output_file_table)
+if __name__ == '__main__':
+    args = get_args()
+
+    tree = read_tree(args=args)
+
+    bootstrap_mapping = {}
+    assign_node_ids(tree, 1, bootstrap_mapping)
+
+    write_output(args=args, tree=tree)
